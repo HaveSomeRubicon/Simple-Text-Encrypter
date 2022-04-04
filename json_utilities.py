e96a5cb43ebcef00aaa1ce2496bae3fcb3ecfd22
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-04-03 22:42:55 -0400

    Created json utilities

---
+++ json_utilities.py
@@ -0,0 +1,39 @@
+# ╔═╗░░░░░░╔╗░░░░░░░░░░░╔╗░░╔╗░░░░░░░░░░░░░░░░░░░░░
+# ║╬╠╦╦═╦╦╦╝╠╗╔╦╗╔══╦═╗╔╝╠═╗║╚╦╦╗╔╗░░░░░░░░░░░░░░░░
+# ║╔╣╔╣╬║║║╬║╚╣║║║║║║╬╚╣╬║╩╣║╬║║║╠╣░░░░░░░░░░░░░░░░
+# ╚╝╚╝╚═╩═╩═╩═╬╗║╚╩╩╩══╩═╩═╝╚═╬╗║╚╝░░░░░░░░░░░░░░░░
+# ░░░░░░░░░░░░╚═╝░░░░░░░░░░░░░╚═╝░░░░░░░░░░░░░░░░░░
+# ╔╗░╔╗░░░░░░░░╔═══╗░░░░░░░░╔═══╗░░╔╗░░░░░░░░░░░╔╗░
+# ║║░║║░░░░░░░░║╔═╗║░░░░░░░░║╔═╗║░░║║░░░░░░░░░░░║║░
+# ║╚═╝╠══╦╗╔╦══╣╚══╦══╦╗╔╦══╣╚═╝╠╗╔╣╚═╦╦══╦══╦═╗║║░
+# ║╔═╗║╔╗║╚╝║║═╬══╗║╔╗║╚╝║║═╣╔╗╔╣║║║╔╗╠╣╔═╣╔╗║╔╗╬╝░
+# ║║░║║╔╗╠╗╔╣║═╣╚═╝║╚╝║║║║║═╣║║╚╣╚╝║╚╝║║╚═╣╚╝║║║╠╗░
+# ╚╝░╚╩╝╚╝╚╝╚══╩═══╩══╩╩╩╩══╩╝╚═╩══╩══╩╩══╩══╩╝╚╩╝░
+# ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+import json
+from datetime import date
+
+today = date.today()
+
+
+def reset_json(): return write_json_file("""{"ciphers": []}""", "cipher.json")
+
+
+def export_json_file(path):
+    with open(path, "r") as json_file:
+        return json.load(json_file)
+
+
+def write_json_file(file_to_write, destination="cipher.json"):
+    with open(destination, "w") as file:
+        json.dump(file_to_write, file)
+
+
+def append_cipher(title="Untitled", path="cipher.json", cipher="rember to delete me", date=today.strftime("%B %d, %Y")):
+    json_file = export_json_file(path)
+    json_file["ciphers"].append(
+        {"title": title, "date": date, "cipher": cipher})
+    write_json_file(json_file, path)
+
+
+append_cipher("cipher.json")
