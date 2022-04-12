a320da73fc6a3ac00be5e5377a916645301a8d4b
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-04-11 21:00:53 -0400

    added append_encrypted_text function

--- storage_manager.py
+++ storage_manager.py
@@ -36,10 +36,15 @@ def append_cipher(title, cipher, path="my_stuff.json", date=today.strftime("%B %
     json_file["ciphers"].append(
         {"title": title, "date": date, "cipher": cipher})
     write_to_json(json_file, path)
-    
-    
-# def append_encrypted_text(title, encrypted_text, path = "my_stuff.json"):
-#     with open(path,'w')
+
+
+def append_encrypted_text(title, encrypted_text, path="my_stuff.json"):
+    new_json = read_json()
+    new_json["myEncryptedText"].append(
+        {"title": title, "encryptedText": encrypted_text})
+    write_to_json(new_json)
+
+
 def run_checks():
     try:
         with open("my_stuff.json", "r") as json_file:
