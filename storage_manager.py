2b273e3b67c70ff10d27a75888aabb87a5dd2a64
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-04-14 22:06:23 -0400

    Added json formating

--- storage_manager.py
+++ storage_manager.py
@@ -13,22 +13,34 @@
 import json
 from datetime import date
 import cipher_tools
+import pprint
 today = date.today()
+
+
+def format_json(path="my_stuff.json"):
+    with open(path, 'r') as json_file:
+        json_data = json.loads(json_file.read())
+    with open(path, 'w') as json_file:
+        json_file.write(pprint.pformat(json_data).replace("'", '"'))
 
 
 def reset_json(path="my_stuff.json"):
     with open(path, "w") as file:
         file.write('{"ciphers": [], "myEncryptedText": []}')
+    format_json()
 
 
 def read_json(path="my_stuff.json"):
     with open(path, "r") as json_file:
         return json.load(json_file)
+    format_json()
 
 
 def write_to_json(file_to_write, destination="my_stuff.json"):
     with open(destination, "w") as file:
         json.dump(file_to_write, file)
+    format_json()
+
 
 
 def append_cipher(title, cipher, path="my_stuff.json", date=today.strftime("%B %d, %Y")):
@@ -36,6 +48,8 @@ def append_cipher(title, cipher, path="my_stuff.json", date=today.strftime("%B %
     json_file["ciphers"].append(
         {"title": title, "date": date, "cipher": cipher})
     write_to_json(json_file, path)
+    format_json()
+
 
 
 def append_encrypted_text(title, encrypted_text, path="my_stuff.json", date=today.strftime("%B %d, %Y")):
@@ -43,6 +57,7 @@ def append_encrypted_text(title, encrypted_text, path="my_stuff.json", date=toda
     new_json["myEncryptedText"].append(
         {"title": title, "encryptedText": encrypted_text, 'date': date})
     write_to_json(new_json)
+    format_json()
 
 
 def run_checks():
@@ -52,6 +67,8 @@ def run_checks():
                 reset_json()
     except FileNotFoundError:
         reset_json()
+    format_json()
+
 
 if __name__ == "__main__":
     input("WARNING: IF YOU ARE SEEING THIS YOU ARE MOST LIKELY RUNNING THE WRONG SCRIPT!!! PLEASE RUN main.py INSTEAD OF THIS SCRIPT")
