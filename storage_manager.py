e52011a2e8ec40bf9ebce8d148546ca40368f76d
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-04-17 16:55:24 -0400

    Got rid of buggy json formating

--- storage_manager.py
+++ storage_manager.py
@@ -17,29 +17,19 @@ import pprint
 today = date.today()
 
 
-def format_json(path="my_stuff.json"):
-    with open(path, 'r') as json_file:
-        json_data = json.loads(json_file.read())
-    with open(path, 'w') as json_file:
-        json_file.write(pprint.pformat(json_data).replace("'", '"'))
-
-
 def reset_json(path="my_stuff.json"):
     with open(path, "w") as file:
         file.write('{"ciphers": [], "myEncryptedText": []}')
-    format_json()
 
 
 def read_json(path="my_stuff.json"):
     with open(path, "r") as json_file:
         return json.load(json_file)
-    format_json()
 
 
 def write_to_json(file_to_write, destination="my_stuff.json"):
     with open(destination, "w") as file:
         json.dump(file_to_write, file)
-    format_json()
 
 
 def append_cipher(title, cipher, path="my_stuff.json", date=today.strftime("%B %d, %Y")):
@@ -47,7 +37,6 @@ def append_cipher(title, cipher, path="my_stuff.json", date=today.strftime("%B %
     json_file["ciphers"].append(
         {"title": title, "date": date, "cipher": cipher})
     write_to_json(json_file, path)
-    format_json()
 
 
 def append_encrypted_text(title, encrypted_text, path="my_stuff.json", date=today.strftime("%B %d, %Y")):
@@ -55,7 +44,6 @@ def append_encrypted_text(title, encrypted_text, path="my_stuff.json", date=toda
     new_json["myEncryptedText"].append(
         {"title": title, "encryptedText": encrypted_text, 'date': date})
     write_to_json(new_json)
-    format_json()
 
 
 def run_checks():
@@ -65,7 +53,6 @@ def run_checks():
                 reset_json()
     except FileNotFoundError:
         reset_json()
-    format_json()
 
 
 if __name__ == "__main__":
