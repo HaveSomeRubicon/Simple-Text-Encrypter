1e05630cdaf76c395e70c6735822e1c901e06bb5
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-05-24 20:38:43 -0400

    renamed ciphers.json to keys.json

--- keys_manager.py
+++ keys_manager.py
@@ -1,6 +1,7 @@
 import json
 
+
 def reset_json(path: str):
     with open(path, 'w') as json_file:
         json_file.write('')
-        json.dump({'ciphers': []}, json_file, indent=2)
+        json.dump({'keys': []}, json_file, indent=2)
