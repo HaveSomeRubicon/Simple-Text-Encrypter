a9244b57661c0c521fbc451f8c504251027a40ce
HaveSomeRubicon <97649192+HaveSomeRubicon@users.noreply.github.com>
2022-05-26 16:32:15 +0000

    Cleaned up debugging stuff

--- keys_manager.py
+++ keys_manager.py
@@ -1,7 +1,7 @@
 import json
 
 
-def reset_json(path: str):
+def reset_json(path: str = 'keys.json'):
     with open(path, 'w') as json_file:
         json_file.write('')
         json.dump({'keys': []}, json_file, indent=2)
